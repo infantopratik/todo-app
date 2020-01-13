25ccff886fc694f0be823d6d224dda391651e956
Pratik <infanto.pratik@gmail.com>
2020-01-13 19:26:38 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class TodoApp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: [
+        {
+          text: "Wash the car",
+          isCompleted: false
+        },
+        {
+          text: "Clean the room",
+          isCompleted: false
+        },
+        {
+          text: "Take Milo for a walk",
+          isCompleted: false
+        }
+      ],
+      newTodo: ""
+    };
+  }
+
+  updateNewTodo = e => {
+    this.setState({
+      newTodo: e.target.value
+    });
+  };
+
+  addNewTodo = () => {
+    const todos = this.state.todos;
+    todos.push({ text: this.state.newTodo, isCompleted: false });
+    this.setState({
+      todos,
+      newTodo: ""
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <p onClick={() => console.log("Test")}>Todo App</p>
+        <ul>
+          {this.state.todos.map(todo => (
+            <li>{todo.text}</li>
+          ))}
+        </ul>
+        <input onChange={this.updateNewTodo} value={this.state.newTodo} />
+        <button onClick={this.addNewTodo}>Add Todo</button>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default TodoApp;
